5b95e7034fe4f3348b43b5b3a5da308815134566
lvguanjun <lvguanjun135@foxmail.com>
2023-11-02 14:54:20 +0800

    improve: Randomize token refresh intervals for increased realism
    cherry-picked from https://github.com/lvguanjun/copilot_share/commit/980bacd92ffa57fc6bba05078fdb5f0e728f2470
    Upstream Issue: https://github.com/lvguanjun/copilot_share/issues/8
    
    Signed-off-by: lvguanjun <lvguanjun135@foxmail.com>

--- utils/cache.py
+++ utils/cache.py
@@ -9,6 +9,7 @@
 
 
 import time
+import random
 
 tokens = {}
 
@@ -18,7 +19,10 @@ def set_token_to_cache(github_token, copilot_token):
 
 
 def get_token_from_cache(github_token):
+    # 当前过期时间30分钟，15-25分钟内随机值刷新
+    # [(30 - 25) * 60, (30 - 15) * 60]
+    extra_time = random.randint(300, 900)
     if copilot_token := tokens.get(github_token):
-        if copilot_token["expires_at"] > int(time.time()) + 60:
+        if copilot_token["expires_at"] > int(time.time()) + extra_time:
             return copilot_token
     return None
