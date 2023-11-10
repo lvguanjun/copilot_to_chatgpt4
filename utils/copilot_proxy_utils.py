818b193764dccd4c93c917668c530d91f1a1d198
lvguanjun <lvguanjun135@foxmail.com>
2023-11-11 01:03:37 +0800

    a temp commit

--- utils/copilot_proxy_utils.py
+++ utils/copilot_proxy_utils.py
@@ -8,14 +8,47 @@
 """
 
 
+import hashlib
+import ipaddress
 from typing import Tuple
 
 from fastapi import Request
 
+from config import IS_SHARE, TOKEN_POOL, g_token_pool, g_token_pool_errors, TOKEN_MAX_ERROR
 from utils.utils import g_vscode_headers_instance, get_copilot_token
 
 
+def select_token(ip: str, token_pool: list) -> str:
+    # 将IP地址转换为整数
+    ip_int = int(ipaddress.ip_address(ip))
+
+    # 计算哈希值
+    hash_obj = hashlib.md5()
+    hash_obj.update(str(ip_int).encode())
+    hash_value = int(hash_obj.hexdigest(), 16)
+    return token_pool[hash_value % len(token_pool)]
+
+
+def check_remove_token(token: str) -> None:
+    if token not in g_token_pool_errors:
+        return
+    g_token_pool_errors[token] += 1
+    if g_token_pool_errors[token] > TOKEN_MAX_ERROR:
+        g_token_pool.remove(token)
+        del g_token_pool_errors[token]
+
+
 async def get_tokens(request: Request) -> Tuple[int, str]:
+    if IS_SHARE:
+        token_pool = g_token_pool.copy()
+        while token_pool:
+            github_token = select_token(request.client.host, token_pool)
+            status_code, copilot_token = await get_copilot_token(github_token, TOKEN_POOL[github_token])
+            if status_code == 200:
+                return 200, copilot_token.get("token")
+            token_pool.remove(github_token)
+            check_remove_token(github_token)
+        return 401, "all token is invalid"
     github_token = request.headers.get("Authorization", "").strip("Bearer ")
     if not github_token:
         return 401, "Unauthorized"
