723bbe1b4ee4a7f9439203e123cf5b024b676329
lvguanjun <lvguanjun135@foxmail.com>
2024-03-08 00:49:44 +0800

    Fix example config error

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,8 @@ update_config "COPILOT_CHAT_URL" "COPILOT_CHAT_URL"
 update_config "COPILOT_CHAT_ROUTE" "COPILOT_CHAT_ROUTE"
 update_config "GITHUB_TOKEN_URL" "GITHUB_TOKEN_URL"
 update_config "SALT" "SALT"
-update_config "TOKEN_URLS_TYPE" "TOKEN_URLS_TYPE"
-update_config "TOKEN_URLS" "TOKEN_URLS"
+update_config "WHITE_TOKEN_URLS" "WHITE_TOKEN_URLS"
+update_config "BLACK_TOKEN_URLS" "BLACK_TOKEN_URLS"
 update_config "RATE_LIMIT_TIME" "RATE_LIMIT_TIME"
 
 # Start Uvicorn with live reload
